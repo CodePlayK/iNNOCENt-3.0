b257205e755ab8a53dbb977ad910404c4a58ecf0
CodePlay <47710564+CodePlayK@users.noreply.github.com>
2026-08-10 08:52:50 +0800

    debug层

--- core/common/component/state_manager_ui/StateManagerUI.gd
+++ core/common/component/state_manager_ui/StateManagerUI.gd
@@ -1,14 +1,12 @@
 extends MarginContainer
+class_name InGameDebugBox
 var drag_on:bool
 var base_postion:Vector2
 var base_mouse_postion:Vector2
 @onready var line: Line2D = $Line2D
 @onready var obj_name: Label = $MarginContainer/VBoxContainer/MarginContainer/HBoxContainer/ObjName
-@onready var option_button: OptionButton = $MarginContainer/VBoxContainer/MarginContainer/HBoxContainer/OptionButton
-@onready var button: Button = $MarginContainer/VBoxContainer/MarginContainer/HBoxContainer/Button
-@onready var label: Label = $MarginContainer/VBoxContainer/MarginContainer/HBoxContainer/Label
+@onready var state_option: OptionButton = $MarginContainer/VBoxContainer/MarginContainer/HBoxContainer/StateOption
 @export var start_state_name:String = "chase"
-@export var target:Node2D
 ##在行内的位置,x=左侧比例,y=右侧比例
 @export var x_stretch_ratio:Vector2 = Vector2(1,10)
 @export var weight_machine_ui:Node2D
@@ -17,36 +15,50 @@ var current_state
 var state_machine:NpcStateManager
 var index_state:Dictionary
 @onready var node_2d: Node2D = $MarginContainer/VBoxContainer/MarginContainer/HBoxContainer/ObjName/Node2D
+@onready var state_label: Label = $MarginContainer/VBoxContainer/MarginContainer/HBoxContainer/StateLabel
 
+func set_line() -> void:
+	# Label 屏幕位置（中心）
+	var label_pos := obj_name.get_global_rect().get_center()
+	
+	# 把角色世界坐标转成屏幕坐标
+	var target_screen_pos := get_viewport().get_canvas_transform() * npc.global_position
+	
+	# 设置点（同样转成 Line2D 本地坐标）
+	line.clear_points()
+	line.add_point(line.to_local(label_pos))
+	line.add_point(line.to_local(target_screen_pos))
+	
 func init(sm:NpcStateManager):
 	npc = sm.npc
 	state_machine = sm
-	option_button.clear()
+	state_option.clear()
 	obj_name.text = npc.name
 	var i =0
 	for state in state_machine.all_states:
-		option_button.add_item(state.name)
+		state_option.add_item(state.name)
 		index_state[state.name]= i
 		i+=1
-	if option_button.item_count>0:
+	if state_option.item_count>0:
 		if index_state.has(start_state_name):
-			option_button.select(index_state[start_state_name])
+			state_option.select(index_state[start_state_name])
 		else :
-			option_button.select(11)
+			state_option.select(11)
 	EventBus._add_debug(self,x_stretch_ratio,30)
 		
 func _process(delta: float) -> void:
 	if !npc:return
-	obj_name.text = npc.name
 	if drag_on:
 		position=base_postion + get_viewport().get_mouse_position()-base_mouse_postion
-	line.set_point_position(1,node_2d.to_local(npc.get_screen_transform().origin))
+	if line.visible:
+		set_line()
+	#line.set_point_position(1,node_2d.to_local(npc.get_screen_transform().origin))
 	if state_machine:
 		if current_state != state_machine.current_state.name:
-			label.text = state_machine.current_state.name
+			state_label.text = state_machine.current_state.name
 func _on_button_pressed() -> void:
 	if state_machine:
-		state_machine.string2state(option_button.get_item_text(option_button.selected),self)
+		state_machine.string2state(state_option.get_item_text(state_option.selected),self)
 
 
 func _on_margin_container_gui_input(event: InputEvent) -> void:
