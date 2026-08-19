9ecfbc3c01e0fd1f2cad0230717195a1fbf749a7
CodePlay <47710564+CodePlayK@users.noreply.github.com>
2026-08-20 01:44:31 +0800

    SettingDialog

--- addons/Cutscener/main/SettingDialog.gd
+++ addons/Cutscener/main/SettingDialog.gd
@@ -2,9 +2,10 @@
 extends AcceptDialog
 
 func _on_confirmed() -> void:
-	CutscenerGlobal.METHOD_BUSES = CutscenerGlobal.CONFIG_DATA_DIC["method_bus"]
-	CutscenerGlobal.STATE_BUSES = CutscenerGlobal.CONFIG_DATA_DIC["state_bus"]
+	CutscenerGlobal.METHOD_BUSES = CutscenerGlobal.CONFIG_DATA_DIC["method_bus"].duplicate()
+	CutscenerGlobal.STATE_BUSES = CutscenerGlobal.CONFIG_DATA_DIC["state_bus"].duplicate()
 	CutscenerGlobal.load_all_method_state_from_global.emit()
 
 func _on_canceled() -> void:
-	_on_confirmed()
+	# 取消不写入，仅重新刷新 UI 勾选状态
+	CutscenerGlobal.refresh_setting_autoload_config.emit()
