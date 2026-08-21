88b99990314f6accd26f71e5c4a5023f11eb8a58
CodePlay <47710564+CodePlayK@users.noreply.github.com>
2026-08-21 21:11:52 +0800

    idle run

--- core/player/states/landing.gd
+++ core/player/states/landing.gd
@@ -12,6 +12,11 @@ func is_animation_play():
 func enter() -> BaseState:
 	if PlayerState.max_height > 150 and not PlayerState.last_state is PlayerAttackState:
 		await player.aniplayer.animation_finished
+	move = get_movement_input_x()
+	if move != 0 and not is_player_blocked():
+		if Input.is_action_pressed("run"):
+			return run_state
+		return walk_state
 	return idle_state
 
 
