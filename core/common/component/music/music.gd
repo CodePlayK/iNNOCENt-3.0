6ffea29e07a74ea776f4a4a0d488eef2156cb0d5
CodePlay <47710564+CodePlayK@users.noreply.github.com>
2026-09-03 11:46:43 +0800

    Optimize Music system: fix SE pool drop bugs, assign audio buses, add global BGM/SE/Ambient volume control

--- core/common/component/music/music.gd
+++ core/common/component/music/music.gd
@@ -1,197 +1,305 @@
 extends Node
-@onready var BGMPlayer=$BGM/backgroundMusicPlayer
-@onready var SE=$SE
-@onready var SE_LOOP= $SE_LOOP
-@onready var BGM= $BGM
-var lock=false
-var index = 1
-#BGM
-var BGM_dic={
- "piano_happy":[preload("res://core/common/sound/BGM/daily/piano-happy.mp3")],
- "slowly":[preload("res://core/common/sound/BGM/daily/slowly-129845.mp3")],
- "by_the_side_of_a_spring":[preload( "res://core/common/sound/BGM/daily/by_the_side_of_a_spring-131449.mp3")],
-}
-#SE
-var SE_dic={
- "light_switch_off":[preload("res://core/common/sound/SE/light_on.mp3")],
- "light_switch_on":[preload("res://core/common/sound/SE/light_off.mp3")],
- "door_close":[preload("res://core/common/sound/SE/door-opening-and-closing-18398_iCigwa2W.mp3"),-20],
- "corridor_door_close":[preload("res://core/common/sound/SE/heavy-mechancial-door-open-6934.mp3")],
- "bubble":[preload("res://core/common/sound/SE/气泡.mp3")],
- "steel_clash":[preload("res://core/common/sound/SE/steelclash.mp3"),-7],
- "be_hit_by_body":[preload("res://core/common/sound/SE/behitbybody.mp3"),-10],
- "talking":[preload("res://core/common/sound/SE/talking.mp3")],
- "slash1":[preload("res://core/common/sound/SE/slash1.mp3")],
- "slash2":[preload("res://core/common/sound/SE/slash2.mp3")],
- "slash3":[preload("res://core/common/sound/SE/slash3.mp3")],
- "slash4":[preload("res://core/common/sound/SE/slash4.mp3")],
- "slash5":[preload("res://core/common/sound/SE/slash5.mp3")],
- "slash6":[preload("res://core/common/sound/SE/slash6.mp3")],
- "slash7":[preload("res://core/common/sound/SE/slash7.mp3")],
- "punch":[preload("res://core/common/sound/SE/punch.mp3")],
- "cut-body":[preload("res://core/common/sound/SE/cut-body.mp3" )],
- "knife-stab":[preload("res://core/common/sound/SE/knife-stab.mp3")],
- "lazer":[preload("res://core/common/sound/SE/lazer.mp3")],
- "foot_step": [preload("res://core/common/sound/SE/footsteps-sneakers-on-tile-running-33003_amTjc1NI.mp3" ),-10,true],
- "bare_foot_step":[preload( "res://core/common/sound/SE/19-pasos-nina-ver2-29199_qZzBtE62.mp3" ),-5,true],
- "forest-ambient":[preload( "res://core/common/sound/SE/loop/mystic-forest-ambient.mp3" ),-5,false],
- "wind-in-trees":[preload( "res://core/common/sound/SE/loop/wind-in-trees.mp3" ),-5,false],
- "running-in-grass":[preload( "res://core/common/sound/SE/loop/running-in-grass.mp3" ),-5,true],
- "stamina_error":[preload("res://core/common/sound/SE/error-llargs.mp3")],
+## 全局音频管理（Autoload: Music）
+## - BGM / SE（一次性） / SE_LOOP（可随场景停止的循环） / Ambient（环境循环）
+## - 使用对象池，避免「没有空闲播放器就丢音效」
+## - 通过 AudioBus 提供全局音量：背景音乐 / 效果音 / 环境音
 
-}
-#资源，声量，是否在切换场景时停止
-var SE_LOOP_dic={
- "foot_step": [preload("res://core/common/sound/SE/footsteps-sneakers-on-tile-running-33003_amTjc1NI.mp3" ),-10,true],
- "bare_foot_step":[preload( "res://core/common/sound/SE/19-pasos-nina-ver2-29199_qZzBtE62.mp3" ),-5,true],
- "forest-ambient":[preload( "res://core/common/sound/SE/loop/mystic-forest-ambient.mp3" ),-5,false],
- "wind-in-trees":[preload( "res://core/common/sound/SE/loop/wind-in-trees.mp3" ),-5,false],
- "running-in-grass":[preload( "res://core/common/sound/SE/loop/running-in-grass.mp3" ),-5,true],
-}
-var playable_on_transition=["door_close","bubble"]
+const BUS_BGM := "BGM"
+const BUS_SE := "SE"
+const BUS_AMBIENT := "Ambient"
 
-var SE_player ={}
-var SE_LOOP_player ={}
-var BGM_player ={}
+## 配置结构约定：
+## [stream, volume_db=0.0, stop_on_level_change=false, is_ambient=false]
+## is_ambient=true 的循环走 Ambient 总线与池
 
-func _ready():
+var BGM_dic: Dictionary = {
+	"piano_happy": [preload("res://core/common/sound/BGM/daily/piano-happy.mp3")],
+	"slowly": [preload("res://core/common/sound/BGM/daily/slowly-129845.mp3")],
+	"by_the_side_of_a_spring": [preload("res://core/common/sound/BGM/daily/by_the_side_of_a_spring-131449.mp3")],
+}
+
+var SE_dic: Dictionary = {
+	"light_switch_off": [preload("res://core/common/sound/SE/light_on.mp3")],
+	"light_switch_on": [preload("res://core/common/sound/SE/light_off.mp3")],
+	"door_close": [preload("res://core/common/sound/SE/door-opening-and-closing-18398_iCigwa2W.mp3"), -20.0],
+	"corridor_door_close": [preload("res://core/common/sound/SE/heavy-mechancial-door-open-6934.mp3")],
+	"bubble": [preload("res://core/common/sound/SE/气泡.mp3")],
+	"steel_clash": [preload("res://core/common/sound/SE/steelclash.mp3"), -7.0],
+	"be_hit_by_body": [preload("res://core/common/sound/SE/behitbybody.mp3"), -10.0],
+	"talking": [preload("res://core/common/sound/SE/talking.mp3")],
+	"slash1": [preload("res://core/common/sound/SE/slash1.mp3")],
+	"slash2": [preload("res://core/common/sound/SE/slash2.mp3")],
+	"slash3": [preload("res://core/common/sound/SE/slash3.mp3")],
+	"slash4": [preload("res://core/common/sound/SE/slash4.mp3")],
+	"slash5": [preload("res://core/common/sound/SE/slash5.mp3")],
+	"slash6": [preload("res://core/common/sound/SE/slash6.mp3")],
+	"slash7": [preload("res://core/common/sound/SE/slash7.mp3")],
+	"punch": [preload("res://core/common/sound/SE/punch.mp3")],
+	"cut-body": [preload("res://core/common/sound/SE/cut-body.mp3")],
+	"knife-stab": [preload("res://core/common/sound/SE/knife-stab.mp3")],
+	"lazer": [preload("res://core/common/sound/SE/lazer.mp3")],
+	"foot_step": [preload("res://core/common/sound/SE/footsteps-sneakers-on-tile-running-33003_amTjc1NI.mp3"), -10.0],
+	"bare_foot_step": [preload("res://core/common/sound/SE/19-pasos-nina-ver2-29199_qZzBtE62.mp3"), -5.0],
+	"stamina_error": [preload("res://core/common/sound/SE/error-llargs.mp3")],
+}
+
+## 循环音效（脚步等会随切关停止）；环境音 is_ambient=true
+var SE_LOOP_dic: Dictionary = {
+	"foot_step": [preload("res://core/common/sound/SE/footsteps-sneakers-on-tile-running-33003_amTjc1NI.mp3"), -10.0, true, false],
+	"bare_foot_step": [preload("res://core/common/sound/SE/19-pasos-nina-ver2-29199_qZzBtE62.mp3"), -5.0, true, false],
+	"running-in-grass": [preload("res://core/common/sound/SE/loop/running-in-grass.mp3"), -5.0, true, false],
+	"forest-ambient": [preload("res://core/common/sound/SE/loop/mystic-forest-ambient.mp3"), -5.0, false, true],
+	"wind-in-trees": [preload("res://core/common/sound/SE/loop/wind-in-trees.mp3"), -5.0, false, true],
+}
+
+@onready var _bgm_root: Node = $BGM
+@onready var _se_root: Node = $SE
+@onready var _loop_root: Node = $SE_LOOP
+@onready var _ambient_root: Node = $Ambient
+
+## free pools
+var _bgm_free: Array[AudioStreamPlayer] = []
+var _se_free: Array[AudioStreamPlayer] = []
+var _loop_free: Array[AudioStreamPlayer] = []
+var _ambient_free: Array[AudioStreamPlayer] = []
+
+## active: key -> player
+var _bgm_active: Dictionary = {} # effect_name -> AudioStreamPlayer
+var _se_active: Dictionary = {} # owner|effect -> AudioStreamPlayer
+var _loop_active: Dictionary = {} # effect_name -> AudioStreamPlayer
+var _ambient_active: Dictionary = {} # effect_name -> AudioStreamPlayer
+
+var _level_change_lock: bool = false
+var _na_counter: int = 0
+
+## 线性音量缓存 0.0~1.0（供设置 UI 读写）
+var bgm_volume_linear: float = 1.0
+var se_volume_linear: float = 1.0
+var ambient_volume_linear: float = 1.0
+
+
+func _ready() -> void:
+	_collect_pool(_bgm_root, _bgm_free, BUS_BGM)
+	_collect_pool(_se_root, _se_free, BUS_SE)
+	_collect_pool(_loop_root, _loop_free, BUS_SE)
+	_collect_pool(_ambient_root, _ambient_free, BUS_AMBIENT)
+
 	EventBus.play_SE.connect(_on_play_SE)
 	EventBus.play_SE_LOOP.connect(_on_play_SE_LOOP)
 	EventBus.play_BGM.connect(_on_play_BGM)
-	EventBus.change_level.connect(_preset)
-	for se_player in SE.get_children():
-		SE_player[se_player.get_name()]=se_player
-	for se_player in SE_LOOP.get_children():
-		SE_LOOP_player[se_player.get_name()]=se_player
-	for se_player in BGM.get_children():
-		BGM_player[se_player.get_name()]=se_player
-	pass 
-func _preset(room):
-	lock=true
-	for player in SE_LOOP_player.keys():
-		if SE_LOOP_player[player].playing and SE_LOOP_dic[player][2]:
-			SE_LOOP_player[player].stop()
-			SE_LOOP_player["NA-"+str(player)]=SE_LOOP_player[str(player)]
-			SE_LOOP_player.erase(player)
-			#Debug.dprintinfo(DebugCT.dp("重置音效:[%s]" %player,self))
-	lock=false
-func _on_play_BGM_daily(bgm_name):
-	BGMPlayer.set_stream(BGM_dic.get(bgm_name))
-	BGMPlayer.play()
-	
-func _on_play_SE(effect_name,speed:float=1.0,effect_volume:float=0.0,owner_name:String="NA",state:bool = true):
-	if lock:return
-	if !SE_dic.has(effect_name):
-		#Debug.dprint("未找到该SE_LOOP-%s"%effect_name)
-		return
-	var k_name = owner_name+"|"+effect_name
-	if state:
-		if SE_player.has(k_name) :
-			if SE_player[k_name].playing:
-				set_se_audio_player(effect_name,k_name,speed,effect_volume)	
-				#Debug.dprint("播放已有[播放中]音效:%s" %k_name)
-			else:
-				SE_player[k_name].set_stream(SE_dic.get(effect_name)[0])
-				set_se_audio_player(effect_name,k_name,speed,effect_volume)	
-				SE_player[k_name].play()
-				#Debug.dprint(DebugCT.dp("播放已有[停止中]音效:%s" %k_name,self))
-		else:
-			for se_key in SE_player.keys():
-				if !SE_player[se_key].playing:
-					SE_player[k_name]=SE_player[se_key]
-					SE_player[k_name].set_stream(SE_dic.get(effect_name)[0])
-					set_se_audio_player(effect_name,k_name,speed,effect_volume)	
-					SE_player[k_name].play()
-					SE_player.erase(se_key)
-					#Debug.dprint("播放新音效:%s" %k_name)
-					break
-		#Debug.dprint("播放循环音效-%s" %effect_name)
-	else:
-		if SE_player.has(k_name):
-			index+=1
-			SE_player["NA-"+str(index)]=SE_player[k_name]
-			SE_player[k_name].stop()
-			SE_player.erase(k_name)
-			#Debug.dprint("停止音效:%s" %k_name)
-			#Debug.dprint("停止循环音效-%s" %)
-		else:
-			#Debug.dprint("未找到要停止的SE！-%s" %effect_name)
-			pass
-	#Debug.dprinterr(str(SE_player.keys()))		
-			
-			
-			
-			
-			
-func _on_play_SE_LOOP(effect_name,state:bool=true,speed:float=1.0,effect_volume:float=0.0):
-	if lock:return
-	if !SE_LOOP_dic.has(effect_name):
-		#Debug.dprint("未找到该SE_LOOP-%s"%effect_name)
-		return
-	if state:
-		if SE_LOOP_player.has(effect_name) :
-			if SE_LOOP_player[effect_name].playing:
-				set_se_loop_audio_player(effect_name,speed,effect_volume)	
-			else:
-				SE_LOOP_player[effect_name].set_stream(SE_LOOP_dic.get(effect_name)[0])
-				set_se_loop_audio_player(effect_name,speed,effect_volume)	
-				SE_LOOP_player[effect_name].play()
-		else:
-			for se_loop_key in SE_LOOP_player.keys():
-				if !SE_LOOP_player[se_loop_key].playing:
-					SE_LOOP_player[effect_name]=SE_LOOP_player[se_loop_key]
-					SE_LOOP_player[effect_name].set_stream(SE_LOOP_dic.get(effect_name)[0])
-					set_se_loop_audio_player(effect_name,speed,effect_volume)	
-					SE_LOOP_player[effect_name].play()
-					SE_LOOP_player.erase(se_loop_key)
-					break
-		#Debug.dprint("播放循环音效-%s" %effect_name)
-	else:
-		if SE_LOOP_player.has(effect_name):
-			SE_LOOP_player[effect_name].stop()
-			SE_LOOP_player["NA-"+str(effect_name)]=SE_LOOP_player[effect_name]
-			SE_LOOP_player.erase(effect_name)
-			#Debug.dprint("停止循环音效-%s" %)
-		else:
-			#Debug.dprint("未找到要停止的SE_LOOP！-%s" %effect_name)
-			pass
-func _on_play_BGM(effect_name,state:bool=true,speed:float=1.0,effect_volume:float=0.0):
-	if !BGM_dic.has(effect_name):
-		#Debug.dprint("未找到该BGM-%s" %effect_name)
-		return
-	if state:
-		for BGM_key in BGM_player.keys():
-			if !BGM_player[BGM_key].is_playing():
-				BGM_player[effect_name]=BGM_player[BGM_key]
-				BGM_player[effect_name].set_stream(BGM_dic.get(effect_name)[0])
-				BGM_player[effect_name].set_pitch_scale(speed)
-				BGM_player[effect_name].set_volume_db(effect_volume)	
-				if BGM_dic.get(effect_name).size()>1&&effect_volume==0:
-					BGM_player[effect_name].set_volume_db(BGM_dic.get(effect_name)[1])
-				BGM_player[effect_name].play()
-				#Debug.dprint("播放BGM-%s" %effect_name)
-				SE_LOOP_player.erase(BGM_key)
-				break
+	EventBus.change_level.connect(_on_change_level)
 
-	else:
-		if BGM_player.has(effect_name):
-			BGM_player[effect_name].stop()
-			BGM_player["NA"]=BGM_player[effect_name]
-			BGM_player.erase(effect_name)
-			#Debug.dprint("停止BGM-%s" %effect_name)
-		else:
-			#Debug.dprint("未找到要停止的BGM！-%s" %effect_name)
-			pass
+	# 确保总线存在（编辑器未同步 layout 时兜底）
+	_ensure_bus(BUS_BGM)
+	_ensure_bus(BUS_SE)
+	_ensure_bus(BUS_AMBIENT)
 
-func set_se_loop_audio_player(effect_name,speed:float=1.0,effect_volume:float=0.0):
-	SE_LOOP_player[effect_name].set_pitch_scale(speed)
-	SE_LOOP_player[effect_name].set_volume_db(effect_volume)
-	if SE_LOOP_dic.get(effect_name).size()>1 and effect_volume==0:
-		SE_LOOP_player[effect_name].set_volume_db(SE_LOOP_dic.get(effect_name)[1])	
-	pass
-func set_se_audio_player(effect_name,k_name,speed:float=1.0,effect_volume:float=0.0):
-	SE_player[k_name].set_pitch_scale(speed)
-	SE_player[k_name].set_volume_db(effect_volume)
-	if SE_dic.get(effect_name).size()>1 and effect_volume==0:
-		SE_player[k_name].set_volume_db(SE_dic.get(effect_name)[1])	
-	SE_player[k_name].play(0)
+	set_bgm_volume(bgm_volume_linear)
+	set_se_volume(se_volume_linear)
+	set_ambient_volume(ambient_volume_linear)
+
+
+func _collect_pool(root: Node, free_list: Array[AudioStreamPlayer], bus_name: String) -> void:
+	for c in root.get_children():
+		if c is AudioStreamPlayer:
+			var p := c as AudioStreamPlayer
+			p.bus = bus_name
+			if not p.finished.is_connected(_on_player_finished.bind(p, free_list)):
+				# 一次性 SE 播放完回收；循环不依赖 finished
+				pass
+			free_list.append(p)
+
+
+func _ensure_bus(bus_name: String) -> void:
+	if AudioServer.get_bus_index(bus_name) == -1:
+		AudioServer.add_bus()
+		var idx := AudioServer.get_bus_count() - 1
+		AudioServer.set_bus_name(idx, bus_name)
+		AudioServer.set_bus_send(idx, "Master")
+
+
+#region Global volume API（线性 0~1）
+func set_bgm_volume(linear: float) -> void:
+	bgm_volume_linear = clampf(linear, 0.0, 1.0)
+	_set_bus_linear(BUS_BGM, bgm_volume_linear)
+
+
+func set_se_volume(linear: float) -> void:
+	se_volume_linear = clampf(linear, 0.0, 1.0)
+	_set_bus_linear(BUS_SE, se_volume_linear)
+
+
+func set_ambient_volume(linear: float) -> void:
+	ambient_volume_linear = clampf(linear, 0.0, 1.0)
+	_set_bus_linear(BUS_AMBIENT, ambient_volume_linear)
+
+
+func get_bgm_volume() -> float:
+	return bgm_volume_linear
+
+
+func get_se_volume() -> float:
+	return se_volume_linear
+
+
+func get_ambient_volume() -> float:
+	return ambient_volume_linear
+
+
+func _set_bus_linear(bus_name: String, linear: float) -> void:
+	var idx := AudioServer.get_bus_index(bus_name)
+	if idx < 0:
+		return
+	AudioServer.set_bus_volume_db(idx, linear_to_db(maxf(linear, 0.0001)))
+	AudioServer.set_bus_mute(idx, linear <= 0.001)
+#endregion
+
+
+func _on_change_level(_room) -> void:
+	_level_change_lock = true
+	# 停止「切关需停」的循环 SE
+	var to_release: Array = []
+	for key in _loop_active.keys():
+		var cfg = SE_LOOP_dic.get(key)
+		if cfg and cfg.size() > 2 and cfg[2]:
+			var p: AudioStreamPlayer = _loop_active[key]
+			p.stop()
+			_loop_free.append(p)
+			to_release.append(key)
+	for k in to_release:
+		_loop_active.erase(k)
+	_level_change_lock = false
+
+
+func _acquire(free_list: Array[AudioStreamPlayer], active: Dictionary, root: Node, bus_name: String) -> AudioStreamPlayer:
+	# 1) 空闲池
+	while free_list.size() > 0:
+		var p: AudioStreamPlayer = free_list.pop_back()
+		if is_instance_valid(p):
+			return p
+	# 2) 回收已停止但仍占 active 的（仅对 SE 有意义）
+	var dead_keys: Array = []
+	for k in active.keys():
+		var ap: AudioStreamPlayer = active[k]
+		if not is_instance_valid(ap) or not ap.playing:
+			dead_keys.append(k)
+			if is_instance_valid(ap):
+				free_list.append(ap)
+	for k in dead_keys:
+		active.erase(k)
+	if free_list.size() > 0:
+		return free_list.pop_back()
+	# 3) 动态扩容，避免丢音效
+	var np := AudioStreamPlayer.new()
+	np.bus = bus_name
+	np.process_mode = Node.PROCESS_MODE_ALWAYS if bus_name != BUS_SE else Node.PROCESS_MODE_INHERIT
+	root.add_child(np)
+	return np
+
+
+func _release_to_pool(player: AudioStreamPlayer, free_list: Array[AudioStreamPlayer], active: Dictionary, key) -> void:
+	if active.has(key) and active[key] == player:
+		active.erase(key)
+	player.stop()
+	player.stream = null
+	if not free_list.has(player):
+		free_list.append(player)
+
+
+func _default_volume_db(cfg: Array, override_db: float) -> float:
+	if override_db != 0.0:
+		return override_db
+	if cfg.size() > 1 and typeof(cfg[1]) in [TYPE_FLOAT, TYPE_INT]:
+		return float(cfg[1])
+	return 0.0
+
+
+func _apply_playback(player: AudioStreamPlayer, stream: AudioStream, pitch: float, vol_db: float) -> void:
+	player.stream = stream
+	player.pitch_scale = pitch
+	player.volume_db = vol_db
+	player.play()
+
+
+#region BGM
+func _on_play_BGM(effect_name: String, state: bool = true, speed: float = 1.0, effect_volume: float = 0.0) -> void:
+	if not BGM_dic.has(effect_name):
+		return
+	if state:
+		if _bgm_active.has(effect_name):
+			var p: AudioStreamPlayer = _bgm_active[effect_name]
+			if p.playing:
+				p.pitch_scale = speed
+				p.volume_db = _default_volume_db(BGM_dic[effect_name], effect_volume)
+				return
+			_apply_playback(p, BGM_dic[effect_name][0], speed, _default_volume_db(BGM_dic[effect_name], effect_volume))
+			return
+		var player := _acquire(_bgm_free, _bgm_active, _bgm_root, BUS_BGM)
+		_bgm_active[effect_name] = player
+		_apply_playback(player, BGM_dic[effect_name][0], speed, _default_volume_db(BGM_dic[effect_name], effect_volume))
+	else:
+		if _bgm_active.has(effect_name):
+			_release_to_pool(_bgm_active[effect_name], _bgm_free, _bgm_active, effect_name)
+#endregion
+
+
+#region SE one-shot
+func _on_play_SE(effect_name: String, speed: float = 1.0, effect_volume: float = 0.0, owner_name: String = "NA", state: bool = true) -> void:
+	if _level_change_lock:
+		return
+	if not SE_dic.has(effect_name):
+		return
+	var k_name := "%s|%s" % [owner_name, effect_name]
+	if state:
+		var player: AudioStreamPlayer
+		if _se_active.has(k_name) and is_instance_valid(_se_active[k_name]):
+			player = _se_active[k_name]
+		else:
+			player = _acquire(_se_free, _se_active, _se_root, BUS_SE)
+			_se_active[k_name] = player
+			# 播完自动回收（避免池枯竭）
+			if not player.finished.is_connected(_on_se_finished):
+				player.finished.connect(_on_se_finished.bind(player, k_name))
+		_apply_playback(player, SE_dic[effect_name][0], speed, _default_volume_db(SE_dic[effect_name], effect_volume))
+	else:
+		if _se_active.has(k_name):
+			_release_to_pool(_se_active[k_name], _se_free, _se_active, k_name)
+
+
+func _on_se_finished(player: AudioStreamPlayer, k_name: String) -> void:
+	if _se_active.get(k_name) == player:
+		_se_active.erase(k_name)
+		if not _se_free.has(player):
+			_se_free.append(player)
+#endregion
+
+
+#region SE_LOOP / Ambient
+func _on_play_SE_LOOP(effect_name: String, state: bool = true, speed: float = 1.0, effect_volume: float = 0.0) -> void:
+	if _level_change_lock:
+		return
+	if not SE_LOOP_dic.has(effect_name):
+		return
+	var cfg: Array = SE_LOOP_dic[effect_name]
+	var is_ambient: bool = cfg.size() > 3 and bool(cfg[3])
+	var free_list := _ambient_free if is_ambient else _loop_free
+	var active := _ambient_active if is_ambient else _loop_active
+	var root := _ambient_root if is_ambient else _loop_root
+	var bus_name := BUS_AMBIENT if is_ambient else BUS_SE
+
+	if state:
+		if active.has(effect_name) and is_instance_valid(active[effect_name]):
+			var p: AudioStreamPlayer = active[effect_name]
+			p.pitch_scale = speed
+			p.volume_db = _default_volume_db(cfg, effect_volume)
+			if not p.playing:
+				p.stream = cfg[0]
+				p.play()
+			return
+		var player := _acquire(free_list, active, root, bus_name)
+		active[effect_name] = player
+		_apply_playback(player, cfg[0], speed, _default_volume_db(cfg, effect_volume))
+	else:
+		if active.has(effect_name):
+			_release_to_pool(active[effect_name], free_list, active, effect_name)
+#endregion
--- core/common/component/sound_effect/sound_effect.gd
+++ core/common/component/sound_effect/sound_effect.gd
@@ -2,9 +2,13 @@ extends Component
 class_name SoundEffect
 @onready var master: Node = %Master
 
-func play_se(sound_config:SoundEffectConfig,obj):
-	if !sound_config.start_time == 0.0:
+func play_se(sound_config: SoundEffectConfig, obj) -> void:
+	if sound_config == null:
+		return
+	if sound_config.start_time != 0.0:
 		await get_tree().create_timer(sound_config.start_time).timeout
-	#if obj is NpcsBaseState or BaseState:
-		#if !master.obj.state_manager.current_state == obj:return
-	EventBus._play_SE(sound_config.se_name,sound_config.se_speed,sound_config.se_pitch,str(owner.get_instance_id()+obj.get_instance_id()))
+	var owner_id := str(owner.get_instance_id() + obj.get_instance_id())
+	# EventBus: name, speed(pitch_scale 用 speed), volume_db, owner, state
+	# 使用 se_pitch 作为播放速率更符合原字段语义；volume 用 se_volume_db
+	var pitch := sound_config.se_pitch if sound_config.se_pitch != 0.0 else sound_config.se_speed
+	EventBus._play_SE(sound_config.se_name, pitch, sound_config.se_volume_db, owner_id, true)
